77cfd939bdcb8d90d270045801c736ad3b1e3e02
waniaadil <aadil688@gmail.com>
2019-04-02 17:45:51 +0530

    HTTP POST

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,4 @@
-
-
-
-    from datetime import datetime
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect, session, abort, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
